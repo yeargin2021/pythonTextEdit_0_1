5b3952d46f70c5daeddbcd4722c96506f9a479a1
Tommy H Yeargin Jr <yeargin2021@gmail.com>
2024-07-28 03:47:01 -0400

    edit

--- mod01.py
+++ mod01.py
@@ -1,7 +1,13 @@
+import datetime
+
 filename = input("Enter a file name: ")
 filename = filename + '.txt'
 
 file = open(filename, "a+")
+# Print current date and time
+file.write("\nCurrent date and time: " + str(datetime.datetime.now()))
+
+
 def runMe():
     saySomething = input("Say something: ")
     file.write('\n' + saySomething)
